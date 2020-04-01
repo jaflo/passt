f023c4fa5fe20fd231cb14cfc4bf2c26b1a39461
Gabriel Britain <quontas@gmail.com>
2020-03-31 20:46:47 -0500

    Added test cases to spite Florian

--- test/db/card.test.ts
+++ test/db/card.test.ts
@@ -28,4 +28,115 @@ describe("Card", () => {
       assert.equal(count, 1);
     }
   });
+
+  describe("isASet", () => {
+    it("should be true when the cards form a set", async () => {
+      const cards = [
+        await Card.create({
+          fillStyle: FillStyle.EMPTY,
+          color: Color.BLUE,
+          shape: Shape.CIRCLE,
+          number: 1,
+        }),
+        await Card.create({
+          fillStyle: FillStyle.EMPTY,
+          color: Color.BLUE,
+          shape: Shape.CIRCLE,
+          number: 2,
+        }),
+        await Card.create({
+          fillStyle: FillStyle.EMPTY,
+          color: Color.BLUE,
+          shape: Shape.CIRCLE,
+          number: 3,
+        })
+      ];
+      assert.equal(Card.isASet(...cards), true);
+    });
+    it("should be false when the cards don't form a set", async () => {
+      const cards = [
+        await Card.create({
+          fillStyle: FillStyle.EMPTY,
+          color: Color.BLUE,
+          shape: Shape.CIRCLE,
+          number: 1,
+        }),
+        await Card.create({
+          fillStyle: FillStyle.LINED,
+          color: Color.BLUE,
+          shape: Shape.CIRCLE,
+          number: 1,
+        }),
+        await Card.create({
+          fillStyle: FillStyle.EMPTY,
+          color: Color.BLUE,
+          shape: Shape.CIRCLE,
+          number: 3,
+        })
+      ];
+      assert.equal(Card.isASet(...cards), false);
+    });
+  });
+
+  describe("containsASet", () => {
+    it("should be true when the cards contain a set", async () => {
+      const cards = [
+        await Card.create({
+          fillStyle: FillStyle.EMPTY,
+          color: Color.RED,
+          shape: Shape.TRIANGLE,
+          number: 2,
+        }),
+        await Card.create({
+          fillStyle: FillStyle.EMPTY,
+          color: Color.RED,
+          shape: Shape.TRIANGLE,
+          number: 3
+        }),
+        await Card.create({
+          fillStyle: FillStyle.FILLED,
+          color: Color.BLUE,
+          shape: Shape.CIRCLE,
+          number: 2,
+        }),
+        await Card.create({
+          fillStyle: FillStyle.LINED,
+          color: Color.GREEN,
+          shape: Shape.SQUARE,
+          number: 2,
+        })
+      ];
+      assert.equal(Card.containsASet(...cards), true);
+    });
+
+    it("should be false when the cards don't contain a set", async () => {
+      const cards = [
+        await Card.create({
+          fillStyle: FillStyle.EMPTY,
+          color: Color.RED,
+          shape: Shape.TRIANGLE,
+          number: 1,
+        }),
+        await Card.create({
+          fillStyle: FillStyle.EMPTY,
+          color: Color.RED,
+          shape: Shape.TRIANGLE,
+          number: 2,
+        }),
+        await Card.create({
+          fillStyle: FillStyle.LINED,
+          color: Color.RED,
+          shape: Shape.TRIANGLE,
+          number: 3
+        }),
+        await Card.create({
+          fillStyle: FillStyle.EMPTY,
+          color: Color.GREEN,
+          shape: Shape.CIRCLE,
+          number: 1
+        })
+      ];
+      assert.equal(Card.containsASet(...cards), false);
+    })
+  })
 });
