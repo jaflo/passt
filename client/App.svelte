c457078672ec09546c2a6c773fc792dce86431e8
Florian <imjaflo@gmail.com>
2020-04-01 19:45:34 -0500

    Remove extraneous

--- client/App.svelte
+++ client/App.svelte
@@ -1,10 +1,7 @@
 <script>
-	import HomeView from "./screens/HomeView.svelte";
-	import JoinView from "./screens/JoinView.svelte";
+	import HomeView from "./home/HomeView.svelte";
 	import GameView from "./game/GameView.svelte";
 	import { socket } from "./connectivity.js";
-
-	// can be in setup, tutorial, or playing
 
 	let roomCode = new URLSearchParams(window.location.search).get("room") || "";
 
