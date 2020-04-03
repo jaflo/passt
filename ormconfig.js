b3189967eb6b5768bd29850dbcdd944858426f1d
Gabriel Britain <quontas@gmail.com>
2020-04-03 14:51:53 -0500

    Updated test GitHub action, and fixed ormconfig.

--- ormconfig.js
+++ ormconfig.js
@@ -2,14 +2,9 @@ const PostgressConnectionStringParser = require('pg-connection-string');
 if (!process.env.DATABASE_URL) {
    console.error("Please supply the DATABASE_URL environment variable");
 }
-const connectionOptions = PostgressConnectionStringParser.parse(process.env.DATABASE_URL);
 module.exports = {
    type: "postgres",
-   host: connectionOptions.host,
-   port: connectionOptions.port || 5432,
-   username: connectionOptions.user,
-   password: connectionOptions.password,
-   database: connectionOptions.database,
+   url: process.env.DATABASE_URL,
    entities: ["server/entity/*.entity.ts"],
    migrations: ["server/migrations/*.ts"],
    cli: {
--- test/testSetup.test.ts
+++ test/testSetup.test.ts
@@ -1,12 +1,9 @@
 import 'mocha';
 import { createConnection, getConnection } from 'typeorm';
-import { Shape, FillStyle, Color, Card } from '../server/entity/card.entity';
 import { seedCards } from '../server/seedCards';
 
 beforeEach(async () => {
-  const connection = await createConnection();
-  const queryRunner = connection.createQueryRunner();
-  await queryRunner.createDatabase('passtTest', true);
+  await createConnection();
 });
 
 afterEach(async () => {
